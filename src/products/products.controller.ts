a9681a1d6ad998e8460bedad85cae9fce4c9548c
W0Ifyyy <bartosz.wilk.123@hotmail.com>
2025-01-16 23:42:36 +0100

    added basic creating and getting products mechanism, with todos for later

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from 'src/dtos/CreateProductDto';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private productService: ProductsService) {}
   @Get()
   getProducts() {
-    return null;
+    return this.productService.getProducts();
+  }
+  @Get(':id')
+  getProductById(@Param('id') id: number) {
+    return this.productService.getProductsById(id);
+  }
+  @Post()
+  createProduct(@Body() createProductParams: CreateProductDto) {
+    return this.productService.createProduct(createProductParams);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateProductDto } from 'src/dtos/CreateProductDto';
 import { Product } from 'src/typeorm/entities/Product';
 import { Repository } from 'typeorm';
 
@@ -9,11 +10,20 @@ export class ProductsService {
     @InjectRepository(Product) private productRepository: Repository<Product>,
   ) {}
   async getProducts() {
+    // todo add security checks like what if products doesnt exist
+
     return await this.productRepository.find();
   }
   async getProductsById(id: number) {
+    // todo add security checks like what if product doesnt exist
     return await this.productRepository.findOne({
       where: { id },
     });
+  }
+  async createProduct(params: CreateProductDto) {
+    //todo create interface for this, and create security checks like if post already exist etc.
+    let newProduct = await this.productRepository.create({ ...params });
+    let productCreation = await this.productRepository.save(newProduct);
+    return { msg: 'product created succesfully!' };
   }
 }
