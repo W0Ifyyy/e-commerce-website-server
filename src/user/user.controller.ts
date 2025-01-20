929a0f05540db64b7b4262e7790249cf99c653c2
W0Ifyyy <bartosz.wilk.123@hotmail.com>
2025-01-20 23:09:16 +0100

    Created service for getting all users and specific user by id

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+  @Get()
+  getUsers() {
+    return this.userService.getAllUsers();
+  }
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.getUserById(id);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/User';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+  async getAllUsers() {
+    let users = await this.usersRepository.find();
+    return users;
+  }
+  async getUserById(id: number) {
+    let user = await this.usersRepository.findOne({ where: { id } });
+    if (!user)
+      throw new HttpException(
+        `User with ID ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    return user;
+  }
+}
