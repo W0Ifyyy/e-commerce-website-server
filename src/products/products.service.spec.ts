667ddb7bab7a7cf77f17dcb6702c44ccd0520f58
W0Ifyyy <bartosz.wilk.123@hotmail.com>
2025-10-29 22:25:14 +0100

    add unit tests for getProductById method; include cases for valid product retrieval and couple of error scenarious

--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -12,21 +12,18 @@ describe('ProductsService', () => {
   let productRepository: Repository<Product>;
 
   const mockProductRepository = {
-    find: jest.fn(),       
-    findOne: jest.fn(),    
-    create: jest.fn(),      
-    save: jest.fn(),        
-    delete: jest.fn(),      
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+    delete: jest.fn(),
   };
 
-  
   const mockCategoryService = {
     getCategoryById: jest.fn(),
   };
 
-  
   beforeEach(async () => {
-    
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         ProductsService,
@@ -51,7 +48,6 @@ describe('ProductsService', () => {
   });
 
   describe('getProducts', () => {
-    
     // TEST 1: Normal case - products exist
     it('should return all products with relations', async () => {
       const mockProducts = [
@@ -110,4 +106,48 @@ describe('ProductsService', () => {
       );
     });
   });
+  describe("getProductById", () => {
+    // TEST 1: Normal case - product exists
+    it("should return a product with given id", async () => {
+      const mockProduct = {
+        id: 1,
+        name: "Laptop",
+        description: "Fancy laptop",
+        price: 999,
+        orderItems: [],
+      }
+      mockProductRepository.findOne.mockResolvedValue(mockProduct);
+      const result = await service.getProductById(mockProduct.id);
+
+      expect(result).toEqual(mockProduct);
+      expect(mockProductRepository.findOne).toHaveBeenCalled();
+      expect(mockProductRepository.findOne).toHaveBeenCalledWith({
+        relations: ['orderItems'],
+        where: { id: mockProduct.id }
+      })
+
+      expect(mockProductRepository.findOne).toHaveBeenCalledTimes(1);
+    })
+    // Test 2 - Error - Product does not exist
+    it("should throw error when there are no products found", async () => {
+      mockProductRepository.findOne.mockResolvedValue(null);
+      await expect(service.getProductById(999)).rejects.toThrow(new HttpException("There is no product with this id", HttpStatus.NOT_FOUND));
+      expect(mockProductRepository.findOne).toHaveBeenCalled();
+    })
+    // Test 3 - Error - Id is zero
+    it("should throw error when the id is zero", async () => {
+      await expect(service.getProductById(0)).rejects.toThrow(new HttpException("Invalid product ID", HttpStatus.BAD_REQUEST));
+      expect(mockProductRepository.find).not.toHaveBeenCalled();
+    })
+    // Test 4 - Error - Id is negative
+    it("should throw error when the id is negative", async () => {
+      await expect(service.getProductById(-1)).rejects.toThrow(new HttpException("Invalid product ID", HttpStatus.BAD_REQUEST));
+      expect(mockProductRepository.find).not.toHaveBeenCalled();
+    })
+    //Test 5 - Error - Id is null
+    it("should throw error when the id is zero", async () => {
+      await expect(service.getProductById(null)).rejects.toThrow(new HttpException("Invalid product ID", HttpStatus.BAD_REQUEST));
+      expect(mockProductRepository.find).not.toHaveBeenCalled();
+    })
+  })
 });
